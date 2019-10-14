f88f168cdbf3faa9cc760e3dcfa189111b3d4d39
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-10-14 12:15:26 +0200

    init

---
+++ src/main/java/dusanjovanov/reactjsandspringrest/ReactjsAndSpringRestApplication.java
@@ -0,0 +1,13 @@
+package dusanjovanov.reactjsandspringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactjsAndSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactjsAndSpringRestApplication.class, args);
+	}
+
+}
