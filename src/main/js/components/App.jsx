30c211ba7b461d03ba701f9202c1d62f6e59c5c5
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-10-18 12:46:11 +0200

    pagination done

--- src/main/js/components/App.jsx
+++ src/main/js/components/App.jsx
@@ -1,29 +1,53 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Button, Container, Header, Modal } from "semantic-ui-react";
+import {
+  Button,
+  Container,
+  Form,
+  Header,
+  Modal,
+  Pagination,
+  Select
+} from "semantic-ui-react";
 import AddEmployeeForm from "./AddEmployeeForm";
 import EmployeeList from "./EmployeeTable";
 
 const App = () => {
   const [employees, setEmployees] = useState([]);
   const [schema, setSchema] = useState(null);
+  const [pagination, setPagination] = useState({
+    totalPages: -1,
+    currentPage: -1
+  });
+  const [pageSize, setPageSize] = useState(1);
   const [isModalVisible, setIsModalVisible] = useState(false);
 
-  useEffect(() => {
-    axios
-      .get("/api/employees?size=10")
-
+  const loadData = pageNumber => {
+    return axios
+      .get(`/api/employees?page=${pageNumber}&size=${pageSize}`)
       .then(res => {
         setEmployees(res.data._embedded.employees);
-        axios
-          .get(res.data._links.profile.href, {
-            headers: {
-              Accept: "application/schema+json"
-            }
-          })
-          .then(res => setSchema(res.data));
+        setPagination({
+          totalPages: res.data.page.totalPages,
+          currentPage: res.data.page.number
+        });
+        return res.data;
       });
-  }, [setEmployees]);
+  };
+
+  const loadSchema = data => {
+    axios
+      .get(data._links.profile.href, {
+        headers: {
+          Accept: "application/schema+json"
+        }
+      })
+      .then(res => setSchema(res.data));
+  };
+
+  useEffect(() => {
+    loadData(0).then(loadSchema);
+  }, [pageSize]);
 
   const onSubmit = data => {
     axios.post("/api/employees", data).then(() => {
@@ -37,14 +61,42 @@ const App = () => {
   const onClickAdd = () => setIsModalVisible(true);
   const onCloseModal = () => setIsModalVisible(false);
 
+  const onPageChange = (e, data) => {
+    loadData(data.activePage - 1);
+  };
+
+  const onChangePageSize = e => {
+    setPageSize(e.currentTarget.value);
+  };
+
   return (
     <Container>
       <Header as="h1">Employees</Header>
-      <Modal
-        open={isModalVisible}
-        onClose={onCloseModal}
-        trigger={<Button onClick={onClickAdd}>Add Employee</Button>}
-      >
+      <Button onClick={onClickAdd}>Add Employee</Button>
+      <Form>
+        <div style={{ marginTop: "1em" }}>
+          <Form.Field
+            label="Num. of rows"
+            control="select"
+            width={5}
+            inline
+            value={pageSize}
+            onChange={onChangePageSize}
+          >
+            <option value={1}>1</option>
+            <option value={5}>5</option>
+          </Form.Field>
+        </div>
+      </Form>
+      <EmployeeList employees={employees} />
+      {pagination.totalPages > 1 && (
+        <Pagination
+          totalPages={pagination.totalPages}
+          activePage={pagination.currentPage + 1}
+          onPageChange={onPageChange}
+        />
+      )}
+      <Modal open={isModalVisible} onClose={onCloseModal}>
         <Modal.Header>Add Employee</Modal.Header>
         <Modal.Content>
           <Modal.Description>
@@ -52,7 +104,6 @@ const App = () => {
           </Modal.Description>
         </Modal.Content>
       </Modal>
-      <EmployeeList employees={employees} />
     </Container>
   );
 };
